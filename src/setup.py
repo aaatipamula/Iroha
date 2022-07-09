e776fd689aab1e6d8b072f345e4d2845cf6dbddb
Aniketh A <aaatipamula@gmail.com>
2022-07-09 12:22:50 -0500

    syntax errors

--- src/setup.py
+++ src/setup.py
@@ -1,28 +1,28 @@
 import json
 
 def main():
-  while True:
+    while True:
         token = input("Please input your bot token here: ")
 
         if token != '':
             break
         else: print('\nPlease enter your bot token!')
 
-  while True:
+    while True:
         dump_channel = input("Please input the channel id here: ")
 
         if dump_channel != '':
             break
         else: print('\nPlease enter your bot token!')
 
-  while True:
+    while True:
         command_prefix = input("Please input the command prefix you would like here: ")
 
         if command_prefix != '':
             break
         else: print('\nPlease enter your bot token!')
 
-  while True:
+    while True:
         about_me = input("Please input the about me you would like here: ")
 
         if about_me != '':
@@ -37,11 +37,11 @@ def main():
     }
 
     with open('./src/data.json', 'w') as f:
-        f.write(json.dumps(x, indent=4))
+        f.write(json.dumps(data, indent=4))
         
     print('''\nYour data is saved in ./src/data.json and can be edited if any of the previously input information is incorrect or needs to be updated!''')    
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
 else:
     print("Please run this as the main file!")
