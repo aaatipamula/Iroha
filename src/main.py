0ce18e86c104cb98e728d883aec10fe998878656
aaatipamula <aaatipamula@gmail.com>
2022-07-14 16:35:17 +0000

    commented out example functions

--- src/main.py
+++ src/main.py
@@ -18,30 +18,30 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="testing..."))
 
 #Simple command, asynchronus fuction that sends a "HELLO WORLD" message to any user who calls command.
-@client.command()
-async def example(ctx):
-    await ctx.send(embed=embeds.embed_a('HELLO WORLD'))
+# @client.command()
+# async def example(ctx):
+#     await ctx.send(embed=embeds.embed_a('HELLO WORLD'))
 
 #Error handling specifically for the example() function if the function calls for any additional arguments they can be handled with the 'isinstance(error, commands.MissingRequiredArgument)' if statement for any missing arguments and so on. 
-@example.error
-async def example_error(ctx, error):
+# @example.error
+# async def example_error(ctx, error):
 
-    if isinstance(error, commands.MissingRequiredArgument):
-        msg = '```Please enter the missing argument!```'
-        await ctx.send(msg)
-    else:
-        await ctx.send(f"```{error}```")
+#     if isinstance(error, commands.MissingRequiredArgument):
+#         msg = '```Please enter the missing argument!```'
+#         await ctx.send(msg)
+#     else:
+#         await ctx.send(f"```{error}```")
 
 #General error handling for all commands, if a command does not have error handling explicitly called this function will handle all errors.
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, NotFound):
-        ctx.send(embed=embeds.cmd_error("This is not a command!"))
+# @client.event
+# async def on_command_error(ctx, error):
+#     if isinstance(error, NotFound):
+#         ctx.send(embed=embeds.cmd_error("This is not a command!"))
 
-    else:
-        print(error)
-        err = client.get_channel(data.get("dump_channel"))
-        await err.send(f"```Error: {error}\nMessage: {ctx.message.content}\nAuthor: {ctx.author}\nServer: {ctx.message.guild}\nLink: {ctx.message.jump_url}\nTraceback: {''.join(tb.format_exception(None, error, error.__traceback__))}```")
+#     else:
+#         print(error)
+#         err = client.get_channel(data.get("dump_channel"))
+#         await err.send(f"```Error: {error}\nMessage: {ctx.message.content}\nAuthor: {ctx.author}\nServer: {ctx.message.guild}\nLink: {ctx.message.jump_url}\nTraceback: {''.join(tb.format_exception(None, error, error.__traceback__))}```")
 
 #A function that runs on every message sent.
 @client.event
@@ -52,7 +52,7 @@ async def on_message(message):
         return
         
     #process any commands before on message event is processed
-    await client.process_commands(message)
+    # await client.process_commands(message)
 
 if __name__ == '__main__':
     client.run(data.get('token'))
